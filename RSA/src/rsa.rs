6d379f24f364b0a9b57381c2a39961770e9eef59
garnold <gustav.arnold@uni-jena.de>
2024-02-08 22:20:25 +0100

    Initial commit

---
+++ RSA/src/rsa.rs
@@ -0,0 +1,11 @@
+pub mod rsa {
+    use num_bigint::BigUint;
+    use num_traits::{Zero, One};
+
+    pub fn multiply (x: &BigUint, m: &i32, n: &BigUint) -> BigUint {
+        let y: BigUint = One::one();
+        
+
+        y
+    }
+}
