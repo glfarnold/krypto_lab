8385a7632130c6a4d00c5e8e8d0f0882a4581ab0
garnold <gustav.arnold@uni-jena.de>
2024-03-03 13:26:33 +0100

    finished and tested

--- Additiv/src/add.rs
+++ Additiv/src/add.rs
@@ -1,10 +1,29 @@
 pub mod add {
-    pub fn encrypt(plaintext: &String, k: &u8) -> String {
+    use std::fs;
+    use std::{env, fs::File};
+    use std::io::Write;
+
+    pub fn encrypt(plaintext: &String, k: &i32) -> String {
+        add_key(plaintext, k)
+    }
+
+    pub fn decrypt_with_key(plaintext: &String, k: &i32) -> String {
+        add_key(plaintext, &-k)
+    }
+
+    fn add_key(plaintext: &String, k: &i32) -> String {
+        let mut key: u8 = 0;
+        if *k < 0 {
+            key = (26 + k) as u8;
+        }
+        else {
+            key = *k as u8;
+        }
         let mut ascii_vec: Vec<u8> = plaintext.chars().map(|c| c as u8).collect();
         for i in 0..ascii_vec.len() {
             if 65 <= ascii_vec[i] && ascii_vec[i] < 91 {
                 ascii_vec[i] -= 65;
-                ascii_vec[i] = (ascii_vec[i] + k) % 26;
+                ascii_vec[i] = (ascii_vec[i] + key) % 26;
                 ascii_vec[i] += 65;
             }
         }
@@ -14,17 +33,8 @@ pub mod add {
 
     pub fn decrypt(crypttext: &String) -> String {
         let max_index: u8 = find_max(crypttext) as u8;
-        let key: u8 = (max_index - 4) % 26;
-        let mut ascii_vec: Vec<u8> = crypttext.chars().map(|c| c as u8).collect();
-        for i in 0..ascii_vec.len() {
-            if 65 <= ascii_vec[i] && ascii_vec[i] < 91 {
-                ascii_vec[i] -= 65;
-                ascii_vec[i] = (ascii_vec[i] - key) % 26;
-                ascii_vec[i] += 65;
-            }
-        }
-        let crypttext: String = ascii_vec.into_iter().map(|c| c as char).collect();
-        crypttext
+        let key: i32 = (max_index as i32 - 4) % 26;
+        add_key(crypttext, &(-key))
     }
 
     fn find_max(crypttext: &String) -> usize {
@@ -36,7 +46,6 @@ pub mod add {
                 counter[chars[i] as usize] += 1;
             }
         }
-        println!("{:?}", counter);
         
         let max_index = match counter.iter().enumerate().max_by_key(|(_, &v)| v) {
             Some((index, _)) => index,
@@ -45,5 +54,38 @@ pub mod add {
             }
         };
         max_index
+    }
+
+    pub fn read_args() -> (String, i32, String, i32){
+        let args: Vec<String> = env::args().collect();
+        if args.len() != 5 {
+            panic!("Programm ausführen mit: cargo run -- [input path] key [outout path] mode")
+        }
+        let input_path = args[1].clone();
+        let key: i32 = args[2].parse().unwrap();
+        if key < 0 || key > 25 {
+            panic!("Key als Zahl zwischen 0 und 25 angeben");
+        }
+        let output_path = args[3].clone();
+        let mode: i32 = args[4].parse().unwrap();
+        (input_path, key, output_path, mode)
+    }
+
+    pub fn write_output_to_file(file_path: &str, output: &String) -> Result<(), std::io::Error> {
+        let mut file = File::create(file_path)?; 
+        file.write_all(output.as_bytes())?;
+    
+        Ok(())
+    }
+
+    pub fn read_from_file(file_path: &str) -> String {
+        let file_contents = match fs::read_to_string(file_path) {
+            Ok(contents) => contents,
+            Err(err) => {
+                eprintln!("Error reading file: {}", err);
+                String::new()
             }
+        };
+        file_contents
+    }
 }
--- Additiv/src/main.rs
+++ Additiv/src/main.rs
@@ -1,10 +1,33 @@
-use crate::add::add::{decrypt, encrypt};
+use crate::add::add::*;
 
 mod add;
 
 fn main() {
-    let a = String::from("ICH GEHE HEUTE BADEN");
-    let crypttext = encrypt(&a, &5);
-    println!("{}", encrypt(&a, &5));
-    println!("{}", decrypt(&crypttext));
+    let input_path = read_args().0;
+    let output_path = read_args().2;
+    let key = read_args().1;
+    let mode = read_args().3;
+
+    let _ = match mode {
+        0 => {
+            let plaintext = read_from_file(&input_path);
+            let crypttext = encrypt(&plaintext, &key);
+            let _ = write_output_to_file(&output_path, &crypttext);
+        },
+        1 => {
+            let crypttext = read_from_file(&input_path);
+            let plaintext = decrypt_with_key(&crypttext, &key);
+            let _ = write_output_to_file(&output_path, &plaintext);
+        }, 
+        2 => {
+            let crypttext = read_from_file(&input_path);
+            let plaintext = decrypt(&crypttext);
+            let _ = write_output_to_file(&output_path, &plaintext);
+        }
+        _ => {
+            panic!("Modus als Zahl zwischen 0 und 2 angeben")
+        },
+    };
+
+    
 }
