9b170b6cab9f0d575d2502550101d7a0be16d645
garnold <gustav.arnold@uni-jena.de>
2024-03-04 12:30:19 +0100

    wip

--- Vigenere/src/main.rs
+++ Vigenere/src/main.rs
@@ -2,6 +2,26 @@ use crate::vigenere::vigenere::*;
 mod vigenere;
 
 fn main() {
-    let a: String = String::from("HELLO, WORLD!");
-    println!("{:?}", find_key_length(&a));
+    // let a: String = read_from_file("src/Kryptotext_TAG.txt");
+    // let a_raw = remove_chars(&a.chars().collect());
+    // let a_raw_string = a_raw.iter().collect();
+    // let n = find_key_length(&a);
+    // let key = get_key(&a_raw_string, &n);
+    // println!("{:?}", decrypt(&a, &key.chars().map(|c| c as u8).collect()));
+
+    let b = read_from_file("src/Klartext_1.txt");
+    let k = String::from("ABCDEFG");
+    let _ = write_output_to_file("./output.txt", 
+                            &encrypt(&b, &k.chars().map(|c| c as u8).collect()));
+
+    
+    let c = read_from_file("./output.txt");
+    let a_raw = remove_chars(&c.chars().collect());
+    let a_raw_string = a_raw.iter().collect();
+    let n = find_key_length(&c);
+    let key = get_key(&a_raw_string, &n);
+
+
+    let _ = write_output_to_file("./output2.txt", 
+    &decrypt(&c, &k.chars().map(|c| c as u8).collect()));
 }
--- Vigenere/src/vigenere.rs
+++ Vigenere/src/vigenere.rs
@@ -1,5 +1,5 @@
 pub mod vigenere {
-    use std::ascii;
+    use std::{ascii, fs::{self, File}, io::Write};
 
     pub fn encrypt(plaintext: &String, keys: &Vec<u8>) -> String {
         let n = keys.len();
@@ -8,13 +8,67 @@ pub mod vigenere {
         for i in 0..ascii_vec.len() {
             if 65 < ascii_vec[i] && ascii_vec[i] < 91 {
                 ascii_vec[i] -= 65;
-                ascii_vec[i] += keys[counter % n];
+                ascii_vec[i] = (ascii_vec[i] + keys[counter % n]) % 26;
                 counter += 1;
                 ascii_vec[i] += 65;
             }
         }
         let crypttext: String = ascii_vec.into_iter().map(|c| c as char).collect();
         crypttext
+    }
+
+    pub fn decrypt(crypttext: &String, key: &Vec<u8>) -> String {
+        let n = key.len();
+        let mut counter = 0;
+        let mut ascii_vec: Vec<u8> = crypttext.chars().map(|c| c as u8).collect(); 
+        for i in 0..ascii_vec.len() {
+            if 65 < ascii_vec[i] && ascii_vec[i] < 91 {
+                ascii_vec[i] -= 65;
+                let mut tmp = (ascii_vec[i] as i32 - key[counter % n] as i32) % 26;
+                if tmp < 0 {
+                    tmp = 26 + tmp;
+                }
+                ascii_vec[i] = tmp as u8;
+                counter += 1;
+                ascii_vec[i] += 65;
+            }
+        }
+        let crypttext: String = ascii_vec.into_iter().map(|c| c as char).collect();
+        crypttext
+    }
+
+    pub fn get_key(crypttext: &String, n: &i32) -> String {
+        let texts: Vec<String> = divide_into_blocks(crypttext, n);
+        let mut key: String = String::new();
+        // Häufigkeitsanalyse für jedes x_i
+        for i in 0..texts.len() {
+            let max = find_max(&texts[i]) as u8;
+            let mut key_char: i32 = (max as i32 - 4) % 26;
+            if key_char < 0 {
+                key_char = 26 + key_char;
+            }
+            key.push((key_char as u8 + 65) as char );
+        }
+        key
+    }
+
+    fn find_max(crypttext: &String) -> usize {
+        let mut counter: Vec<i32> = vec![0;26];
+        let mut chars: Vec<u8> = crypttext.chars().map(|c| c as u8).collect();
+        for i in 0..chars.len() {
+            if 65 <= chars[i] && chars[i] < 91 {
+                chars[i] -= 65;
+                counter[chars[i] as usize] += 1;
+            }
+        }
+        
+        let max_index = match counter.iter().enumerate().max_by_key(|(_, &v)| v) {
+            Some((index, _)) => index,
+            None => {  
+                panic!("Crypttext ist leer");
+            }
+        };
+        max_index
     }
 
     fn get_coincidence_index(crypttext: &String) -> f64 {
@@ -54,17 +108,36 @@ pub mod vigenere {
 
     pub fn find_key_length(crypttext: &String) -> i32 {
         let mut ic = vec![0.0;100];
-        for i in 1..3 {
+        for i in 1..101 {
             let texts = divide_into_blocks(crypttext, &i);
             let mut ic_tmp = 0.0;
             for j in 0..i {
                 ic_tmp += get_coincidence_index(&texts[j as usize]);
             }
-            ic[i as usize] = ic_tmp / i as f64;
+            ic[i as usize -1] = ic_tmp / i as f64;
         }
-        ic.sort_by(|a,b| a.partial_cmp(b).unwrap());
-        let biggest_ic: Vec<f64> = ic.iter().rev().take(3).cloned().collect();
-        println!("{:?}", biggest_ic);
-        0
+        let mut indexed_ic: Vec<(usize, &f64)> = ic.iter().enumerate().collect();
+        indexed_ic.sort_by(|a,b| a.1.partial_cmp(b.1).unwrap());
+        let sorted_indices: Vec<usize> = indexed_ic.iter().map(|&(index, _)| index).collect();
+        println!("{:?}", sorted_indices);
+        sorted_indices[sorted_indices.len()-1] as i32
+    }
+
+    pub fn read_from_file(file_path: &str) -> String {
+        let file_contents = match fs::read_to_string(file_path) {
+            Ok(contents) => contents,
+            Err(err) => {
+                eprintln!("Error reading file: {}", err);
+                String::new()
+            }
+        };
+        file_contents
+    }
+
+    pub fn write_output_to_file(file_path: &str, output: &String) -> Result<(), std::io::Error> {
+        let mut file = File::create(file_path)?; 
+        file.write_all(output.as_bytes())?;
+    
+        Ok(())
     }
 }
